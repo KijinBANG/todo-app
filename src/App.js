e34978fac918925771ddc8213a0cffe5f74f36fa
KijinBANG <mr.kijin.bang@gmail.com>
2022-04-30 18:46:05 +0900

    implement todos in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import {useState} from 'react';
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 
 const App = () => {
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            text: 'fundamental issues about REACT',
+            checked: true,
+        },
+        {
+            id: 2,
+            text: 'try to style components',
+            checked: true,
+        },
+        {
+            id: 3,
+            text: 'try to make todo-app',
+            checked: false,
+        },
+    ]);
     return (
         <TodoTemplate>
             <TodoInsert/>
-            <TodoList/>
+            <TodoList todos={todos}/>
         </TodoTemplate>
     );
 }
