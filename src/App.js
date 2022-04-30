f551adce2aa441794ce2832b8799e2f7d2021dce
KijinBANG <mr.kijin.bang@gmail.com>
2022-04-30 18:10:01 +0900

    add TodoList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
 
 const App = () => {
-  return (
-      <TodoTemplate>
-          <TodoInsert />
-      </TodoTemplate>
-  );
+    return (
+        <TodoTemplate>
+            <TodoInsert/>
+            <TodoList/>
+        </TodoTemplate>
+    );
 }
 
 export default App;
