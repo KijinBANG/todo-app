46e56074c8732ca65aa0daf4886b9992dd9b27f7
KijinBANG <mr.kijin.bang@gmail.com>
2022-04-30 19:36:22 +0900

    implement onToggle

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -6,15 +6,17 @@ import {
 import cn from 'classname';
 import './TodoListItem.scss';
 
-const TodoListItem = ({todo, onRemove}) => {
+const TodoListItem = ({todo, onRemove, onToggle}) => {
     const {id, text, checked} = todo;
     return (
         <div className={'TodoListItem'}>
-            <div className={cn('checkbox', {checked})}>
+            <div className={cn('checkbox', {checked})} onClick={() => onToggle(id)}>
                 {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
                 <div className={'text'}>{text}</div>
             </div>
-            <div className={'remove'} onClick={() => onRemove(id)}>
+            <div
+                className={'remove'}
+                onClick={() => onRemove(id)}>
                 <MdRemoveCircleOutline/>
             </div>
         </div>
