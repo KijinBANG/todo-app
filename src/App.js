27b4ea8f9eaf64ee4410cb75cf45c4b66b5df504
KijinBANG <mr.kijin.bang@gmail.com>
2022-05-03 19:14:02 +0900

    add bulk data

--- src/App.js
+++ src/App.js
@@ -3,28 +3,44 @@ import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 
-const App = () => {
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            text: 'fundamental issues about REACT',
-            checked: true,
-        },
-        {
-            id: 2,
-            text: 'try to style components',
-            checked: true,
-        },
-        {
-            id: 3,
-            text: 'try to make todo-app',
+function createBulkTodos() {
+    const array = [];
+    for (let i=1; i<=2500; i++) {
+        array.push({
+            id: i,
+            text: `할 일 ${i}`,
             checked: false,
-        },
-    ]);
+        });
+    }
+    return array;
+}
+
+const App = () => {
+    const [todos, setTodos] = useState(
+    // [
+        // {
+        //     id: 1,
+        //     text: 'fundamental issues about REACT',
+        //     checked: true,
+        // },
+        // {
+        //     id: 2,
+        //     text: 'try to style components',
+        //     checked: true,
+        // },
+        // {
+        //     id: 3,
+        //     text: 'try to make todo-app',
+        //     checked: false,
+        // },
+    // ]
+        createBulkTodos
+    );
 
     //고윳값으로 사용될 id
     //ref 를 사용하여 변수 담기
-    const nextId = useRef(4);
+    // const nextId = useRef(4);
+    const nextId = useRef(2501);
 
     const onInsert = useCallback(
         text => {
